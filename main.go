48275e9a997eab0b5745344dd3375d42cfe3ec2b
tommymaeibezx-sys <tommymaeibezx@gmail.com>
2026-06-18 12:59:12 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
-// main.go
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback local
+	}
+
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Servidor corriendo en puerto:", port)
+
+	err := http.ListenAndServe("0.0.0.0:"+port, nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
